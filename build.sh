b309a3cf33f9e570e71978a365ac0151435b0c28
octakitten <raspberry2425@gmail.com>
2024-05-14 14:51:48 -0400

    adding sphinx documentation

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+echo "Building OpenCB package"
+hatch version b
+FOLDER="dist_beta/"
+hatch build -t wheel $FOLDER
+cd $FOLDER
+NAME=$(ls -t | head -n1)
+VERSION=$(echo $NAME | cut -d'-' -f2)
+mv "$NAME" "opencb-$VERSION.whl"
+echo "Built package: opencb-$VERSION.whl"
+cd ..
+echo "Build complete"
