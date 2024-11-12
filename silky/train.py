6f236d4a76a1ddc323b7857ef7db85f754a0c2be
octakitten <raspberry2425@gmail.com>
2024-11-11 20:15:24 -0500

    putting it in the hyperbolic time chamber

--- silky/train.py
+++ silky/train.py
@@ -8,16 +8,12 @@ import os
 import random
 
 # this isnt used currently. might deleted later...
-class training_options():
-    option = [
-              "Maysee/tiny-imagenet",
-              "",
-              "path/to/data"
-             ]
-    choice = 0
-    hf_repo = "Maysee/tiny-imagenet"
-    repo_url = ""
-    repo_path = "path/to/data"
+def time_chamber():
+    percent = 0.0
+    while percent < .95:
+        train("",  "")
+        percent = test("", "")
+    print("We're so back.")
 
 def train(repo, path):
 
@@ -68,7 +64,6 @@ def train(repo, path):
         n = numbers_to_use.pop(j)
 
         attempts += 1
-        tally = np.zeros(200)
 
         # the value here for exposure is important actually
         # the "exposure time" is the time that the model is given 
@@ -78,8 +73,9 @@ def train(repo, path):
         # longer the exposure time runs. this is something you'll have to
         # figure out a balance for as you work with training models
         exposure_time = 200
+        tally = np.zeros(exposure_time)
         for k in range(0, exposure_time):
-            output = np.array(mdl.update(dataformat[n]["image"]))
+            output = np.array(mdl.update(dataformat[n]["image"].item()))
             tally = tally + output
 
         # see how the model did an log it.
@@ -114,5 +110,77 @@ def train(repo, path):
     mdl.save(progpath)
     return
     
+def test(repo, path):
+    # set up the dataset so it can be used on the gpu
+    gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if repo == "" : repo = "Maysee/tiny-imagenet"
+    dataset = datasets.load_dataset(repo, split="test")
+    dataformat = dataset.with_format("torch", device=gpu)
+    #batchsize = 8
+    #dataloader = DataLoader(dataformat, batch_size=batchsize)
+
+    # set up the save path and event logging
+    if path == "":
+        path = os.getcwd() + "/default/"
+    basepath = path
+    savepath = path + "winners"
+    progpath = path + "in-prog"
+    logfilename = basepath + "testing.log"
+    logging.basicConfig(level=logging.DEBUG, filename=logfilename, filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+    logging.info('Starting a  new run...')
+
+    # initialize the model if we need to, but default to loading it in
+    mdl = model.velvet()
+    try:
+        os.path.exists(savepath + "/width.npy")
+        mdl.load(savepath)
+    except:
+        try:
+            os.path.exists(progpath + "/width.npy")
+            mdl.load(progpath)
+        except:
+            print("Unable to find a model to load!")
+    attempts = 0
+    wins = 0
+
+    # set up tools we need to randomize the data selection process
+    len_dataset = len(dataformat)
+    numbers_to_use = list(range(0, len_dataset))
+
+    # loop over the dataset pseudo randomly
+    for i in range(0, len_dataset):
+        j = random.randint(0, len(numbers_to_use))
+        n = numbers_to_use.pop(j)
+
+        # the value here for exposure is important actually
+        # the "exposure time" is the time that the model is given 
+        # to process and understand each data element.
+        # theoretically, the model should need at least a certain amount of exposure
+        # time in order to make accurate predictions. but it consumes resources the 
+        # longer the exposure time runs. this is something you'll have to
+        # figure out a balance for as you work with training models
+        attempts += 1
+        exposure_time = 200
+        tally = np.zeros(exposure_time)
+        for k in range(0, exposure_time):
+            output = np.array(mdl.update(dataformat[n]["image"].item()))
+            tally = tally + output
+
+        # see how the model did an log it.
+        guess = np.argmax(tally)
+        answer = dataformat[n]["label"].item()
+        logging.info('{ "item# : "' + str(n) + '" }')
+        logging.info('{ "guess" : "' + str(guess) + '" }')
+        logging.info('{ "answer"  : "' + str(answer) + '" }')
+
+        if answer == guess:
+            wins += 1
+            logging.info('WIN! Wins so far: ' + str(wins))
+    logging.info('Run ending...')
+    logging.info('Total wins this run: ' + str(wins))
+    logging.info('Total attempts this run: ' + str(attempts))
+
+    return wins/attempts
 
 
