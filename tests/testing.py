5c0b3c61842c128fdaca5cee2d3b68dfa7cc544c
octakitten <raspberry2425@gmail.com>
2024-11-12 21:57:07 -0500

    minor fixes

--- tests/testing.py
+++ tests/testing.py
@@ -2,5 +2,5 @@ import silky
 from silky import iteration
 from silky import train as tr
 
-opts = tr.optionsobj("ILSVRC/imagenet-1k", "", 256, 256, 500, 500, 200, 2)
+opts = tr.optionsobj("ILSVRC/imagenet-1k", None, "", 256, 256, 500, 500, 200, 2)
 tr.time_chamber(opts)
