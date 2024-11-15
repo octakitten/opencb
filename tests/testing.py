2b165092dff80f5f78d1779ab0feb479b0efb0ea
octakitten <raspberry2425@gmail.com>
2024-11-15 07:08:29 -0500

    switching datasets

--- tests/testing.py
+++ tests/testing.py
@@ -2,5 +2,5 @@ import silky
 from silky import iteration
 from silky import train as tr
 
-opts = tr.optionsobj("", None, "", 256, 256, 500, 500, 200, 2)
+opts = tr.optionsobj("ILSVRC", None, "./testing01", 256, 256, 500, 500, 200, 2)
 tr.time_chamber(opts)
